458edb2ba98ec8728e73cc6faa06bb143999cfab
André Vitor <andrevitor103@gmail.com>
2021-08-19 20:06:20 -0300

    Inicializao dados

---
+++ Calculadora.java
@@ -0,0 +1,6 @@
+class Calculadora {
+    static public void main(String[] args){
+        Teclado teclado = new Teclado();
+        System.out.println("Óla mundo");
+    }
+}
---
+++ Teclado.java
@@ -0,0 +1,16 @@
+class Teclado {
+    private int numero;
+
+    public Teclado(){
+        this.numero = 0;
+    }
+
+    public void setNumero(int numero){
+        this.numero = numero;
+    }
+
+    public void getNumero(){
+       return this.numero; 
+    }
+
+}
